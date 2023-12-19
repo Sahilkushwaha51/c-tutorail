49a302f691bcd714fe216f66ff164534f2523fc9
Sahilkushwaha51 <152473785+Sahilkushwaha51@users.noreply.github.com>
2023-12-19 12:47:09 +0530

    Add files via upload

---
+++ loop/work4.c
@@ -0,0 +1,14 @@
+//print 1 to N
+#include<stdio.h>
+int main(){
+    int i=1,n;
+    printf("\n enter no :");
+    scanf("%d",n);
+    while (i<=n){
+        printf("%d \n",i);
+        i++;    
+    }
+
+    return 0;
+    
+}
