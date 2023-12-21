27f579009b0c88c988c782203e7c8440f35c01d3
Sahilkushwaha51 <152473785+Sahilkushwaha51@users.noreply.github.com>
2023-12-21 20:34:09 +0530

    Add files via upload

---
+++ Temperate/multi.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i,n,multi;
+    printf("enter no :");
+    scanf("%d",&n);
+
+    for (int i = 1; i <= 10; i++)
+    {
+        multi=n*i;
+        printf("\n %d * %d = %d",n,i,multi);
+    }
+    
+    return 0;
+}
